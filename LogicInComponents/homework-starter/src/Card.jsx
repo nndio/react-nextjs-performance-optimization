b49867476980304b15ab04a4ea99c6e4a7072217
vahnovanAnastasia <vahnovananastasia1@gmail.com>
2026-07-27 09:35:02 +0300

    Display Discounted Price
    If a product is on sale, the original price is shown crossed out, while the discounted price is highlighted in color.

--- LogicInComponents/homework-starter/src/Card.jsx
+++ LogicInComponents/homework-starter/src/Card.jsx
@@ -8,12 +8,28 @@ export const Card = ({
   discount
 }) => {
 
-    return (
+  const discountedPrice = discount ? Math.round(price * (1 - discount)) : price;
+
+  return (
     <article className="product-card">
       <img className="product-card__image" src={imageUrl} alt={title} />
 
       <div className="product-card__price-container">
-        {price}
+        {discount ? (
+          <>
+            <span className="product-card__price product-card__price_with-discount">
+              {discountedPrice.toLocaleString()} ₽
+            </span>
+
+            <span className="product-card__price product-card__price_without-discount">
+              {price.toLocaleString()} ₽
+            </span>
+          </>
+        ) : (
+          <span className="product-card__price">
+            {price.toLocaleString()} ₽
+          </span>
+        )}
       </div>
 
       <h2 className="product-card__title">{title}</h2>
