f350a6c934025c2322c96791a1096bc4adefcbdd
vahnovanAnastasia <vahnovananastasia1@gmail.com>
2026-08-06 16:26:25 +0300

    Implement note form validation with React Hook Form and Zod

--- DataInApplication/homework-starter/client/src/components/NoteForm/NoteForm.tsx
+++ DataInApplication/homework-starter/client/src/components/NoteForm/NoteForm.tsx
@@ -1,49 +1,101 @@
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+
 import { FormField } from "../FormField";
 import { Button } from "../Button";
-import "./NoteForm.css";
-import { FC, FormEventHandler, useState } from "react";
-import { useMutation } from "@tanstack/react-query";
 import { createNote } from "../../api/Note";
 import { queryClient } from "../../api/queryClient";
 
+import "./NoteForm.css";
+
 export interface INoteFormProps {}
 
+const CreateNoteSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, "Введите заголовок"),
+
+  text: z
+    .string()
+    .trim()
+    .min(10, "Длина поста должна быть не менее 10 символов"),
+});
+
+type CreateNoteForm = z.infer<typeof CreateNoteSchema>;
+
 export const NoteForm: FC<INoteFormProps> = () => {
-  const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<CreateNoteForm>({
+    resolver: zodResolver(CreateNoteSchema),
+  });
+
+  const [errorMessage, setErrorMessage] = useState<string>();
 
   const createNoteMutation = useMutation(
     {
-      mutationFn: () => createNote(title, text),
+      mutationFn: ({ title, text }: CreateNoteForm) =>
+        createNote(title, text),
+
       onSuccess() {
-        queryClient.invalidateQueries({ queryKey: ["notes"] });
+        queryClient.invalidateQueries({
+          queryKey: ["notes"],
+        });
+
+        setErrorMessage(undefined);
+        reset();
+      },
+
+      onError(error) {
+        setErrorMessage(String(error));
       },
     },
-    queryClient
+    queryClient,
   );
-  
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-
-    createNoteMutation.mutate();
-  }
 
   return (
-    <form className="note-form" onSubmit={handleSubmit}>
-      <FormField label="Заголовок">
+    <form
+      className="note-form"
+      onSubmit={handleSubmit((data) => {
+        createNoteMutation.mutate(data);
+      })}
+    >
+      <FormField
+        label="Заголовок"
+        errorMessage={errors.title?.message}
+      >
         <input
           type="text"
-          value={title}
-          onChange={(event) => setTitle(event.currentTarget.value)}
+          {...register("title")}
         />
       </FormField>
-      <FormField label="Текст">
-        <textarea 
-          value={text}
-          onChange={event => setText(event.currentTarget.value)}
-        />
+
+      <FormField
+        label="Текст"
+        errorMessage={errors.text?.message}
+      >
+        <textarea {...register("text")} />
       </FormField>
-      <Button type="submit" isLoading={createNoteMutation.isPending}>Сохранить</Button>
+
+      {errorMessage && (
+        <span style={{ color: "red" }}>
+          {errorMessage}
+        </span>
+      )}
+
+      <Button
+        type="submit"
+        isLoading={createNoteMutation.isPending}
+      >
+        Сохранить
+      </Button>
     </form>
   );
 };
