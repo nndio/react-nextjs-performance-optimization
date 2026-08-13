1f01bc01d1c5c3dbdf72fee5ddf9fac8b23eec97
vahnovanAnastasia <vahnovananastasia1@gmail.com>
2026-08-13 17:09:05 +0300

    Search function by name and genre

--- Routing/homework-starter/src/pages/PlaylistsPage/PlaylistsPage.tsx
+++ Routing/homework-starter/src/pages/PlaylistsPage/PlaylistsPage.tsx
@@ -1,20 +1,52 @@
 import "./PlaylistsPage.css";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { PLAYLISTS } from "../../data";
+import { ChangeEvent } from "react";
 
 export function PlaylistsPage() {
     
+    const [searchParam, setSearchParam] = useSearchParams();
+    
+    const handleSearchName = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { value } = event.target;
+        setSearchParam({ searchName: value.toLowerCase() });
+    };
+
+    const handleSearchGenre = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { value } = event.target;
+        setSearchParam({ searchGenre: value.toLowerCase() });
+    };
+
+    const searchName = searchParam.get("searchName") || "";
+
+    const searchGenre = searchParam.get("searchGenre") || "";
+
+    const filteredPlaylists = PLAYLISTS.filter(({ name, genre }) => {
+		const matchesName = name.toLowerCase().includes(searchName);
+		const matchesGenre = genre.toLowerCase().includes(searchGenre);
+
+		return matchesName && matchesGenre;
+	});
+
     return (
         <div className="playlistsPage">
             <h2>PlaylistsPage</h2>
 
             <div className="playlists">
+                <label>
+					введите жанр{" "}
+					<input type="text" value={searchGenre} onChange={handleSearchGenre} />
+				</label>
+                <label>
+					введите название{" "}
+					<input type="text" value={searchName} onChange={handleSearchName} />
+				</label>
 
-                {PLAYLISTS.map(({ id, name }) => (
-                    <Link to={`/playlists/${id}`} key={id}>
-                        {name}
-                    </Link>
-                ))}
+                {filteredPlaylists.map(({ id, name }) => (
+					<Link to={`/playlists/${id}`} key={id}>
+						{name}
+					</Link>
+				))}
             </div>
         </div>
     );
